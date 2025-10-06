9c140305fa277b7a2a70bca747c81163fea1baee
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2025-10-06 15:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ st.link_button("SINGIFY", "https://exploreai.streamlit.app/singify")
 
 st.link_button("AI PODCAST", "https://exploreai.streamlit.app/aipodcast")
 
-st.link_button("CSV VISUALISATION", "https://csvvisualisation.streamlit.app")
+st.link_button("CSV ANALYSIS", "https://csvvisualisation.streamlit.app")
 
 
 # st.link_button("Go to MODEL LISTS", "https://ashishraxaul.streamlit.app/list_models")
